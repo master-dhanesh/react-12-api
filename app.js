bdd2330a4733a7c59f0cdbcaf94bf48701f138eb
master-dhanesh <dhanesh1296@gmail.com>
2024-03-13 17:27:44 +0530

    Db connection and atlas

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const app = express();
 
+// db connection
+require("./models/dbconfig").dbconnection();
+
 app.listen(process.env.PORT, () => {
     console.log(`The Server is running on port ${process.env.PORT}`);
 });
