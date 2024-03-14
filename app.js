66c8500656269a0cddff5da969f36d34bf8339f1
master-dhanesh <dhanesh1296@gmail.com>
2024-03-14 17:33:08 +0530

    routes and logger

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const app = express();
 // db connection
 require("./models/dbconfig").dbconnection();
 
+// routes
+const userRouter = require("./routes/userRoute");
+
+// logger
+const logger = require("morgan");
+app.use(logger("tiny"));
+// body pasrser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// http://localhost:8080/api/user/
+// http://localhost:8080/api/user/create
+
+app.use("/api/user", userRouter);
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`The Server is running on port ${process.env.PORT}`);
 });
